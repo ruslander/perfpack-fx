d73ba982f71269730c49e71ffbb1699bde9b757a
Ruslan <ruslan.rusu@yahoo.com>
2019-11-11 00:04:36 -0500

    layout

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,7 @@ require "sinatra/json"
 require 'open3'
 require 'pp'
 require 'json'
+require 'csv'
 require 'base64'
 
 set :protection, except: [ :json_csrf ]
@@ -44,4 +45,58 @@ def eval_usl(table)
 	else
 		raise error
 	end
+end
+
+post '/review' do
+	content_type :json
+
+	begin
+	  	tableArgs = params[:data] 
+		data = eval_data(tableArgs)
+		data.to_json
+	rescue => error
+		pp error.message
+    	halt 500  , error.message
+	end
+end	
+
+def eval_data(table)
+
+	result = []
+	rawData = CSV.parse(table, headers: true)
+
+	x0 = 0;
+
+	rawData.each_with_index do |row, idx|
+		pp idx
+		pp row
+
+		n = row["N"].to_i
+		x = row["X"].to_i
+
+		if idx == 0
+			x0 = x;
+		end	
+
+		cn = (x/(x0.to_f)).round(2)
+		cdivn = (cn/(n.to_f)).round(2)
+		ndivc = ((n.to_f)/cn).round(2)
+		
+		liniarity = n -1;
+		deviation = (((n.to_f)/cn)-1).round(2)
+
+		result << {
+			:n => n,
+			:x => x,
+			:cn => cn,
+			:cdivn => cdivn,
+			:ndivc => ndivc,
+			:liniarity => liniarity,
+			:deviation => deviation
+		}
+	end	
+
+
+
+	result
 end
