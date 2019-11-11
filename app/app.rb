e8fe7e34385dba715e6b924de37ea12009b341cc
Ruslan <ruslan.rusu@yahoo.com>
2019-11-10 22:26:27 -0500

    error reporting

--- app/app.rb
+++ app/app.rb
@@ -2,9 +2,7 @@ require 'sinatra'
 require "sinatra/json"
 require 'open3'
 require 'pp'
-require 'uri'
 require 'json'
-require 'enumerator'
 require 'base64'
 
 set :protection, except: [ :json_csrf ]
@@ -18,42 +16,32 @@ get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
-
 post '/usl' do
-  	table = params[:data] 
-  	
-  	#pp table
-
-  	eval_model(table)
-  	
-	data = IO.read("/tmp/rplot-file.jpg")
-	Base64.encode64(data)
+	begin
+	  	tableArgs = params[:data] 
+		eval_usl(tableArgs)
+	rescue => error
+		pp error.message
+    	halt 500  , error.message
+	end
 end	
 
-
-
-def eval_model(table)
-	result = ""
-	error = ""
-
-	model = IO.read("./usl-calculator.r")
+def eval_usl(table)
+	model = IO.read("./models/usl-calculator.r")
 
 	model = model
 		.gsub("\t", "")	
 		.gsub("TAB_SPEC_NX", table)	
 
-	path_to_file = "/tmp/rplot-file.jpg"
-	#File.delete(path_to_file) if File.exist?(path_to_file)
+	error = ""
 
-	Open3.popen3("Rscript --verbose -e '#{model}'") do |stdin, stdout, stderr, wait_thr|
-	  result = stdout.read
+	Open3.popen3("Rscript -e '#{model}'") do |stdin, stdout, stderr, wait_thr|
 	  error = stderr.read
 	end
 
 	if error.nil? || error.empty?
-  		return result
+		return Base64.encode64(IO.read("/tmp/rplot-file.jpg"))
 	else
-		pp "!!!! #{error}"
-		return error
+		raise error
 	end
 end
